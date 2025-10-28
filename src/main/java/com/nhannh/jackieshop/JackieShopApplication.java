d5a9ddf274132bd2def78fa5b6c67c49890b60a6
Nhan Nguyen Hoang <nhannguyenhoang.hcmus@gmail.com>
2025-10-28 22:53:05 +0700

    Init project structure

---
+++ src/main/java/com/nhannh/jackieshop/JackieShopApplication.java
@@ -0,0 +1,13 @@
+package com.nhannh.jackieshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackieShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JackieShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhannh/jackieshop/JackieShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhannh.jackieshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackieShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
